3e3a9a2b2c00d981617041cec20bde8cd4aec126
innerway-xq <15821966148@163.com>
2023-03-11 20:12:57 +0800

    added support for BuildingNoMoney and BuildingAlreadyBuilt

--- Code/Patches/ButtonText.cs
+++ Code/Patches/ButtonText.cs
@@ -1,9 +1,8 @@
 ﻿namespace ShowBuildingSize
 {
-    using System;
+    using ColossalFramework;
     using ColossalFramework.UI;
     using HarmonyLib;
-    using UnityEngine;
 
 
     [HarmonyPatch(typeof(GeneratedScrollPanel), nameof(GeneratedScrollPanel.OnTooltipEnter))]
@@ -12,8 +11,31 @@
 
         private static void hideButtonText(UIComponent button, UIMouseEventParameter p)
         {
-            if(button != null)
+            if (button != null)
                 (button as UIButton).text = "";
+            PrefabInfo prefabinfo = button.objectUserData as PrefabInfo;
+            UISprite uisprite = button.components[0] as UISprite;
+            if (uisprite != null)
+            {
+                if (button.isEnabled)
+                {
+                    if (!prefabinfo.CanBeBuilt())
+                    {
+                        uisprite.spriteName = "ThumbnailBuildingAlreadyBuilt";
+                        uisprite.enabled = true;
+                    }
+                    else
+                    {
+                        uisprite.spriteName = "ThumbnailBuildingNoMoney";
+                        int constructionCost = prefabinfo.GetConstructionCost();
+                        uisprite.enabled = (Singleton<EconomyManager>.instance.PeekResource(EconomyManager.Resource.Construction, constructionCost) != constructionCost);
+                    }
+                }
+                else
+                {
+                    uisprite.enabled = false;
+                }
+            }
         }
 
         private static void showButtonText(UIButton button)
@@ -29,13 +51,17 @@
 
         static void Postfix(UIMouseEventParameter p)
         {
-            if(p != null && p.source is UIButton)
+            if (p != null && p.source is UIButton)
             {
                 UIButton uibutton = p.source as UIButton;
                 uibutton.eventMouseLeave += hideButtonText;
                 showButtonText(uibutton);
+                UISprite uisprite = uibutton.components[0] as UISprite;
+                if (uisprite != null)
+                {
+                    uisprite.enabled = false;
+                }
             }
-      
         }
     }
 
